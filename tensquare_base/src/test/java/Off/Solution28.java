b5926356c8231a7a34c6bae0e9102073ee12747d
c_shazhengnian <1421805850@qq.com>
2019-06-24 14:36:22 +0800

    查找区间

--- tensquare_base/src/test/java/Off/Solution28.java
+++ tensquare_base/src/test/java/Off/Solution28.java
@@ -4,6 +4,48 @@ package Off;
  * @author ：lyj
  * @email: : iclyj@iclyj.cn
  * @date ：2019/6/24 10:59
+ *
+ * 查找区间
  */
 public class Solution28 {
+
+    /**
+     * 题目
+     *
+     *   Input: nums = [5,7,7,8,8,10], target = 8
+         Output: [3,4]
+
+         Input: nums = [5,7,7,8,8,10], target = 6
+         Output: [-1,-1]
+     */
+
+    public static int[] searchRange(int[] nums, int target){
+        int first = binarySearch(nums, target);
+        int last = binarySearch(nums, target + 1) - 1;
+        if (first == nums.length || nums[first] != target){
+            return new int[]{-1, -1};
+        } else {
+            return new int[]{first,Math.max(first,last)};
+        }
+    }
+
+    private static int binarySearch(int[] nums, int target) {
+        int l = 0, h = nums.length; // 注意h 的初始值
+        while (l < h){
+            int m = l + (h - l) / 2;
+            if (nums[m] >= target){
+                h = m;
+            }else {
+                l = m + 1;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = new int[]{5,7,7,8,8,10};
+        int target = 8;
+        System.out.println(searchRange(nums, target));
+    }
+
 }
