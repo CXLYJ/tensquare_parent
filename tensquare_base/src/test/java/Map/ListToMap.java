76cd54859b096f934d60e9ea6aa79ea88a98215c
c_shazhengnian <1421805850@qq.com>
2019-07-03 16:20:36 +0800

    list转换为map

---
+++ tensquare_base/src/test/java/Map/ListToMap.java
@@ -0,0 +1,30 @@
+package Map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author ：lyj
+ * @email: : iclyj@iclyj.cn
+ * @date ：2019/7/3 11:24
+ *
+ * 将list转为map
+ */
+public class ListToMap {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("123");
+        list.add("321");
+
+        Map<String, String> map = new HashMap<>();
+        for (String s : list){
+            map.put(s,s);
+        }
+        System.out.println(map);
+    }
+
+}
