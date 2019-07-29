330d6af1538d1fc4a77de4924b278d19f20da50f
c_shazhengnian <1421805850@qq.com>
2019-07-29 10:40:09 +0800

    最长递增子序列

--- tensquare_base/src/test/java/leetCode/Solution10.java
+++ tensquare_base/src/test/java/leetCode/Solution10.java
@@ -12,18 +12,25 @@ import java.util.Arrays;
 public class Solution10 {
 
     public int lengthOfLIS(int[] nums){
-        int n = nums.length;
-        int[] dp = new int[n];
-        for (int i = 0; i < n; i++){
-            int max  = 1;
+        int n = nums.length; //定义长度
+        int[] dp = new int[n]; //放进一个一维数组
+        for (int i = 0; i < n; i++){ //循环遍历
+            int max  = 1; //定义初始最大值为1
             for (int j = 0; j < i; j++){
-                if (nums[i] > nums[j]){
-                    max = Math.max(max,dp[j] + 1);
+                if (nums[i] > nums[j]){ //比较前后的值
+                    max = Math.max(max,dp[j] + 1); //返回最大的那个值
                 }
             }
             dp[i] = max;
         }
-        return Arrays.stream(dp).max().orElse(0);
+//        return Arrays.stream(dp).max().orElse(0);  //导致时间过长
+
+        int ret = 0;
+        for (int i = 0; i < n; i++) { //再与从到n比较一遍
+            ret = Math.max(ret, dp[i]); //取出最大的值
+        }
+        return ret;
+
     }
 
 }
