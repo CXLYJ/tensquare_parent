7e850902ba9a7e12ea465b6cb685b2af0ba9c1c6
‘cxlyj <1421805850@qq.com>
2019-09-16 15:17:07 +0800

    '深入学习spring组件祖册'

--- tensquare_bean/src/main/java/com/lyj/config/WebConfig.java
+++ tensquare_bean/src/main/java/com/lyj/config/WebConfig.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
  * @author ：lyj
  * @email: : iclyj@iclyj.cn
  * @date ：2019/9/16
+ *
+ * 通过@Bean注解，我们向IOC容器注册了一个名称为user（Bean名称默认为方法名，
+ * 我们也可以通过@Bean("myUser")方式来将组件名称指定为myUser）
  */
 @Configuration
 public class WebConfig {
