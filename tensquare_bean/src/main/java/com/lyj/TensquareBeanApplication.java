92dbb46654879b095eca4a8a5aa06d1def65eb9a
‘cxlyj <1421805850@qq.com>
2019-09-16 15:10:08 +0800

    '深入学习spring组件祖册'

---
+++ tensquare_bean/src/main/java/com/lyj/TensquareBeanApplication.java
@@ -0,0 +1,13 @@
+package com.lyj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TensquareBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TensquareBeanApplication.class, args);
+    }
+
+}
---
+++ tensquare_bean/src/main/java/com/lyj/entity/User.java
@@ -0,0 +1,21 @@
+package com.lyj.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author ：lyj
+ * @email: : iclyj@iclyj.cn
+ * @date ：2019/9/16
+ */
+@ToString
+@AllArgsConstructor
+@Data
+public class User {
+
+    private String userName;
+
+    private Integer age;
+
+}
