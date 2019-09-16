747930d43eef9987afd1116471180964ca76008f
‘cxlyj <1421805850@qq.com>
2019-09-16 16:06:05 +0800

    nsquare_parent.git

--- tensquare_bean/src/main/java/com/lyj/demo/config/WebConfig.java
+++ tensquare_bean/src/main/java/com/lyj/demo/config/WebConfig.java
@@ -1,7 +1,11 @@
 package com.lyj.demo.config;
 
+import com.lyj.demo.entity.User;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author ：lyj
@@ -20,9 +24,21 @@ import org.springframework.context.annotation.Configuration;
  * 现在我们脱离XML配置后，可以使用@ComponentScan注解来扫描组件并注册。
  * 在使用@ComponentScan扫描之前，我们先创建一个Controller，一个Service，一个Dao，并标注上相应的注解。
  * 然后修改配置类
+ *
+ * 4.
+ * 根据注解来排除（type = FilterType.ANNOTATION）,这些注解的类型为classes = {Controller.class, Repository.class}。
+ * 即Controller和Repository注解标注的类不再被纳入到IOC容器中。
+ * 根据指定类型类排除（type = FilterType.ASSIGNABLE_TYPE），排除类型为User.class，其子类，实现类都会被排除。
+ *
  */
 @Configuration
-@ComponentScan("com.lyj.demo")
+//@ComponentScan("com.lyj.demo")
+@ComponentScan(value = "com.lyj.demo",
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ANNOTATION,
+                                      classes = {Controller.class, Repository.class}),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = User.class)
+        })
 public class WebConfig {
 
 //    @Bean("myUser")
