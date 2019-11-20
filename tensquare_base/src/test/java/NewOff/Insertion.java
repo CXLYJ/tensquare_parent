0e7cc767f9a51a058b25f23af0f5074bfdbb3f2c
‘cxlyj <1421805850@qq.com>
2019-11-20 22:37:15 +0800

    希尔排序

--- tensquare_base/src/test/java/NewOff/Insertion.java
+++ tensquare_base/src/test/java/NewOff/Insertion.java
@@ -13,7 +13,7 @@ public class Insertion<T extends Comparable<T>> extends Sort<T> {
         int N = nums.length;
         for (int i = 1; i < N; i++){
             for (int j = i; j > 0 && less(nums[i], nums[j - 1]); j--){
-                sawp(nums, j, j - 1);
+                swap(nums, j, j - 1);
             }
         }
     }
