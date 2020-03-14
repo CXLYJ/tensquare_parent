0f693dc89e9ccd5e2466fe1a4f9e76508bf01b1e
‘cxlyj <1421805850@qq.com>
2020-03-14 21:21:51 +0800

    oom异常

---
+++ tensquare_base/src/test/java/Thread/ThreadTest.java
@@ -0,0 +1,38 @@
+package Thread;
+
+import lombok.extern.slf4j.Slf4j;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * @author ：lyj
+ * @email: : iclyj@iclyj.cn
+ * @date ：2020/3/14 21:08
+ */
+@Slf4j
+public class ThreadTest {
+
+
+    public static void main(String[] args) throws InterruptedException {
+            ThreadPoolExecutor threadPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(1);
+            for (int i = 0; i < 100000000; i++){
+                threadPool.execute(() -> {
+                    String payload = IntStream.rangeClosed(1,1000000)
+                            .mapToObj(__ -> "a")
+                            .collect(Collectors.joining("")) + UUID.randomUUID().toString();
+                    try {
+                        TimeUnit.HOURS.sleep(1);
+                    } catch (InterruptedException e) {
+                    }
+                    log.info(payload);
+                });
+            }
+            threadPool.shutdown();
+            threadPool.awaitTermination(1,TimeUnit.HOURS);
+    }
+
+}
