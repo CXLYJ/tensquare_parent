33c864546f26129004245292f9d9b58d76f5d009
‘cxlyj <1421805850@qq.com>
2020-03-21 23:14:28 +0800

    剑指offer-

---
+++ tensquare_base/src/test/java/Off/Solution32.java
@@ -0,0 +1,28 @@
+package Off;
+
+/**
+ * @author ：lyj
+ * @email: : iclyj@iclyj.cn
+ * @date ：2020/3/21 23:00
+ */
+public class Solution32 {
+
+    /**
+     * 求连续子数组的最大和
+     *
+     * @param array 数组
+     * @return 最大和
+     */
+    public int FindGreatestSumOfSubArray(int[] array){
+        int n = array.length;
+        int[] res = new int[n];
+        res[0] = array[0];
+        int max = res[0];
+        for (int i = 1; i < n; ++i){
+            res[i] = res[ i - 1] > 0 ? res[ i - 1] + array[i] : array[i];
+            max = Math.max(max, res[i]);
+        }
+        return max;
+    }
+
+}
