a9c2ecfe648578a14884e7fe3981dc92b46f5b43
‘cxlyj <1421805850@qq.com>
2020-03-30 17:21:02 +0800

    线程创建的两种方式

---
+++ tensquare_base/src/test/java/Thread/TraditionalThread.java
@@ -0,0 +1,40 @@
+package Thread;
+
+/**
+ * @author ：lyj
+ * @email: : iclyj@iclyj.cn
+ * @date ：2020/3/30 17:04
+ *
+ * 线程创建方式
+ */
+public class TraditionalThread {
+
+    public static void main(String[] args) {
+        //这哥们的代码写的比较给力
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Runnable:" + Thread.currentThread().getName());
+            }
+        }){
+
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Thread:" + Thread.currentThread().getName());
+            }
+
+        }.start();
+    }
+
+}
