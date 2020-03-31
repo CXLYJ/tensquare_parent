31bce2884ef3a07082a44697c21023fea097814c
‘cxlyj <1421805850@qq.com>
2020-03-31 16:34:38 +0800

    newFixedThreadPool的线程池

---
+++ tensquare_base/src/test/java/Thread/ThreadPool2.java
@@ -0,0 +1,17 @@
+package Thread;
+
+/**
+ * @author ：lyj
+ * @email: : iclyj@iclyj.cn
+ * @date ：2020/3/31 16:33
+ */
+public class ThreadPool2 {
+
+    //线程池的概念与Executors类的使用
+    public static void main(String[] args) {
+        //缓存的线程池
+        //自动根据任务数量来设定线程数去服务，多了就增加线程数，少了就减少线程数
+        //这貌似跟一般情况相同，因为一般也是一个线程执行一个任务，但是这里的好处是：如果有个线程死了，它又会产生一个新的来执行任务
+    }
+
+}
