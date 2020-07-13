2fa8810470c2f45f11033dcff113388a97c52d76
‘cxlyj <1421805850@qq.com>
2020-07-13 22:21:08 +0800

    递归调用树

---
+++ tensquare_base/src/test/java/digui/Menu.java
@@ -0,0 +1,31 @@
+package digui;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author ：lyj
+ * @email: : iclyj@iclyj.cn
+ * @date ：2020/7/13 21:46
+ */
+@Data
+public class Menu {
+
+    private String id;
+    private String parentId;
+    private String text;
+    private String url;
+    private String yxbz;
+    private List<Menu> children;
+
+    public Menu(String id,String parentId,String text,String url,String yxbz) {
+                 this.id=id;
+                 this.parentId=parentId;
+                 this.text=text;
+                 this.url=url;
+                 this.yxbz=yxbz;
+             }
+
+}
---
+++ tensquare_base/src/test/java/digui/MenuTree.java
@@ -0,0 +1,51 @@
+package digui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ：lyj
+ * @email: : iclyj@iclyj.cn
+ * @date ：2020/7/13 21:55
+ */
+public class MenuTree {
+
+    private List<Menu> menuList = new ArrayList<>();
+
+    public MenuTree(List<Menu> menuList){
+        this.menuList = menuList;
+    }
+
+    public List<Menu> buildTree(){
+        List<Menu> treeMenus = new ArrayList<>();
+        for (Menu menuNode : getRootNode()){
+            menuNode = buildChildTree(menuNode);
+            treeMenus.add(menuNode);
+        }
+        return treeMenus;
+    }
+
+    //递归，建立子树结构
+    private Menu buildChildTree(Menu menuNode) {
+        List<Menu> childMenu = new ArrayList<>();
+        menuList.forEach(menu -> {
+            if (menu.getParentId().equals(menuNode.getId())){
+                childMenu.add(buildChildTree(menu));
+            }
+        });
+        menuNode.setChildren(childMenu);
+        return menuNode;
+    }
+
+    //获取根节点
+    private List<Menu> getRootNode() {
+        List<Menu> rootMenuList = new ArrayList<>();
+        menuList.forEach(menu -> {
+            if (menu.getParentId().equals("0")){
+                rootMenuList.add(menu);
+            }
+        });
+        return rootMenuList;
+    }
+
+}
